6d10b55e3019b7e42956a0e823d1354d23259834
plumest <chayathon.k@ku.th>
2020-04-21 15:14:40 +0700

    Create Todo component 📝

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <li class="d-flex align-items-center list-group-item">
+        <span>
+            {{ msg }}
+        </span>
+        <button class="btn btn-outline-primary border-0" ><i class="fas fa-edit"/></button>
+        <button class="btn btn-outline-danger border-0"><i class="fas fa-trash"/></button>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            msg: {
+                type: String,
+                request: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
